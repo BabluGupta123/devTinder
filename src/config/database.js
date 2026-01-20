7492bcbe990726af984206744839ce720a7a84ec
BabluGupta123 <bablu8795379783@gmail.com>
2026-01-20 08:45:31 +0530

    added env file

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,8 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 
 const connectDB = async () => {
-  await mongoose.connect(
-    "mongodb+srv://bablu8795379783:Gupta123@cluster0.t5usjq2.mongodb.net/devTinder?appName=Cluster0"
-  );
+  await mongoose.connect(process.env.MONGODB_URI);
 };
 
 module.exports = { connectDB };
