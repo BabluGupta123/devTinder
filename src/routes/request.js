9f809caf2919403d226dbbeac2617aed9e7e1516
BabluGupta123 <bablu8795379783@gmail.com>
2026-01-23 07:34:09 +0530

    Queries & Index in DB & Handeled Corner cases

--- src/routes/request.js
+++ src/routes/request.js
@@ -2,17 +2,63 @@ const express = require("express");
 const requestRoute = express.Router();
 
 const { userAuth } = require("../middlewares/auth");
+const ConnectionRequest = require("../models/connectionRequest");
+const User = require("../models/user");
 
-requestRoute.post("/connection", userAuth, async (req, res) => {
-  try {
-    const user = await req.user;
-    if (!user) {
-      res.status(404).send("Login Please! ");
+requestRoute.post(
+  "/connection/:status/:toUserId",
+  userAuth,
+  async (req, res) => {
+    try {
+      const fromUserId = req.user._id;
+      const { toUserId, status } = req.params;
+
+      // self request check
+      if (fromUserId.toString() === toUserId) {
+        return res.status(400).send("Try sending request to other users!");
+      }
+
+      // status validation
+      const allowedStatus = ["interested", "ignored"];
+      if (!allowedStatus.includes(status)) {
+        return res.status(400).send("Invalid status");
+      }
+
+      // check user exists
+      const isUserExist = await User.findById(toUserId);
+      if (!isUserExist) {
+        return res.status(404).send("User does not exist!");
+      }
+
+      // check if connection request already exists (both directions)
+      const isConnectionExist = await ConnectionRequest.findOne({
+        $or: [
+          { fromUserId, toUserId },
+          { fromUserId: toUserId, toUserId: fromUserId },
+        ],
+      });
+
+      if (isConnectionExist) {
+        return res.status(400).send("Request already sent!");
+      }
+
+      // create request
+      const request = new ConnectionRequest({
+        fromUserId,
+        toUserId,
+        status,
+      });
+
+      await request.save();
+
+      res.status(201).json({
+        message: "Connection request sent! ",
+        data: request,
+      });
+    } catch (err) {
+      res.status(400).send("Connection request Error: " + err.message);
     }
-    res.status(200).send("Connection request sent by " + user.firstName);
-  } catch (err) {
-    res.send("Connection request Error: " + err.message);
-  }
-});
+  },
+);
 
 module.exports = { requestRoute };
